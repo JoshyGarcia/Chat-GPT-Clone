1d9e6cd7671162c094e16b1b885dd57b1450fffd
Joshy Garcia <joshy.garcia@lc.cuny.edu>
2023-04-28 22:37:18 -0400

    removed maxTokens parameter

--- backend/server.js
+++ backend/server.js
@@ -1,112 +1,109 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
 
 // use cors and allow all origins to access the server
-app.use(cors());
+app.use(cors())
 
 // calling body-parser to handle the Request Object from POST requests
-var bodyParser = require('body-parser');
-const { query } = require('express');
+var bodyParser = require("body-parser")
+const { query } = require("express")
 // parse application/json, basically parse incoming Request Object as a JSON Object
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // start the server
 app.listen(4000, () => {
-  console.log('listening on port 4000');
-});
+  console.log("listening on port 4000")
+})
 
 // Store clients' SSE connections
-const clients = new Map();
+const clients = new Map()
 
 // Handle the SSE endpoint
-app.get('/events/:clientId', (req, res) => {
+app.get("/events/:clientId", (req, res) => {
   try {
-  console.log('Client connected', req.params.clientId);
-  const clientId = req.params.clientId;
+    console.log("Client connected", req.params.clientId)
+    const clientId = req.params.clientId
 
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-  res.flushHeaders();
+    res.setHeader("Content-Type", "text/event-stream")
+    res.setHeader("Cache-Control", "no-cache")
+    res.setHeader("Connection", "keep-alive")
+    res.flushHeaders()
 
-  clients.set(clientId, res);
+    clients.set(clientId, res)
 
-  req.on('close', () => {
-    clients.delete(clientId);
-  });
+    req.on("close", () => {
+      clients.delete(clientId)
+    })
   } catch (error) {
     console.error(error)
   }
-});
+})
 
 const sendToClient = (clientId, message) => {
-  const client = clients.get(clientId);
+  const client = clients.get(clientId)
   if (client) {
-    console.log('Sending to client', clientId, message);
-    client.write(`data: ${JSON.stringify(message)}\n\n`);
+    console.log("Sending to client", clientId, message)
+    client.write(`data: ${JSON.stringify(message)}\n\n`)
   }
-};
+}
 
-app.post('/', async (req, res) => {
-
+app.post("/", async (req, res) => {
   try {
-  console.log('Request body', req.body);
-  const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${req.body.userApiKey}`
-    },
-    body: JSON.stringify({
-      model: req.body.model,
-      messages: req.body.messages,
-      max_tokens: req.body.maxTokens,
-      temperature: req.body.temperature,
-      stream: true,
+    console.log("Request body", req.body)
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${req.body.userApiKey}`,
+      },
+      body: JSON.stringify({
+        model: req.body.model,
+        messages: req.body.messages,
+        temperature: req.body.temperature,
+        stream: true,
+      }),
+    }).catch((error) => {
+      console.error(error)
+      return res.status(500).json({ error: "Something went wrong" })
     })
-  }).catch((error) => {
-    console.error(error)
-    return res.status(500).json({ error: 'Something went wrong' })
-  })
 
-  if (!response.ok) {
-    console.error(response.statusText)
-    return res.status(500).json({ error: 'Something went wrong' })
-  }
+    if (!response.ok) {
+      console.error(response.statusText)
+      return res.status(500).json({ error: "Something went wrong" })
+    }
 
-  const reader = response.body.getReader()
-  const decoder = new TextDecoder('utf-8')
+    const reader = response.body.getReader()
+    const decoder = new TextDecoder("utf-8")
 
-  while (true) {
-    const { value } = await reader.read()
+    while (true) {
+      const { value } = await reader.read()
 
-    const chunk = decoder.decode(value)
-    const transformedChunk = chunk
-      .split('\n')
-      .filter(Boolean)
-      .map((line) => line.replace('data: ', '').trim())
+      const chunk = decoder.decode(value)
+      const transformedChunk = chunk
+        .split("\n")
+        .filter(Boolean)
+        .map((line) => line.replace("data: ", "").trim())
 
+      for (const data of transformedChunk) {
+        if (data === "[DONE]") {
+          console.log("Done")
+          // sendToClient(req.body.clientId, { done: true });
+          return res.end("data: [DONE]\n\n")
+        }
 
-    for (const data of transformedChunk) {
-      if (data === '[DONE]') {
-        console.log('Done')
-        // sendToClient(req.body.clientId, { done: true });
-        return res.end('data: [DONE]\n\n') 
-      }
-
-      try {
-        const json = JSON.parse(data)
-        const { content } = json.choices?.[0]?.delta
-        content && sendToClient(req.body.clientId, content); // Send the response to the appropriate SSE client
-      } catch (error) {
-        console.error(error)
+        try {
+          const json = JSON.parse(data)
+          const { content } = json.choices?.[0]?.delta
+          content && sendToClient(req.body.clientId, content) // Send the response to the appropriate SSE client
+        } catch (error) {
+          console.error(error)
+        }
       }
     }
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: "Something went wrong" })
   }
-} catch (error) {
-  console.error(error)
-  return res.status(500).json({ error: 'Something went wrong' })
-  }
-});
+})
--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,110 +1,117 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { Dropdown } from './Dropdown'
-import { nanoid } from 'nanoid'
-import { Range } from './Range'
+import React from "react"
+import { useState, useEffect } from "react"
+import { Dropdown } from "./Dropdown"
+import { nanoid } from "nanoid"
+import { Range } from "./Range"
 
 export const Chat = () => {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState("")
   const [messages, setMessages] = useState([])
-  const [temperature, setTemperature] = useState(0.7 || localStorage.getItem('temperature') || 0.7)
-  const [maxTokens, setMaxTokens] = useState(localStorage.getItem('maxTokens') || 50);
 
-  const [clientId, setClientId] = useState('')
-  const [model, setModel] = React.useState('gpt-4-0314')
-  const [userApiKey, setUserApiKey] = useState('' || localStorage.getItem('userApiKey'))
+  const [temperature, setTemperature] = useState(
+    0.7 || localStorage.getItem("temperature") || 0.7
+  )
+  const [maxTokens, setMaxTokens] = useState(
+    localStorage.getItem("maxTokens") || 50
+  )
+  const [userApiKey, setUserApiKey] = useState(
+    localStorage.getItem("userApiKey") || ""
+  )
+  const [model, setModel] = React.useState("gpt-4-0314")
+
+  const [clientId, setClientId] = useState("")
   const [messagesHtml, setMessagesHtml] = useState([])
-  const messagesContainerRef = React.useRef(null);
-
+  const messagesContainerRef = React.useRef(null)
 
   const changeTemperature = (event) => {
-    setTemperature(parseFloat(event.target.value));
-  };
+    setTemperature(parseFloat(event.target.value))
+  }
 
-  const changeMaxTokens = (event) => {
-    setMaxTokens(parseInt(event.target.value));
-  };
-
-  const handleModelChange = (event) => {
+  const changeModel = (event) => {
     setModel(event.target.value)
   }
 
   useEffect(() => {
-    localStorage.setItem('temperature', temperature)
+    localStorage.setItem("temperature", temperature)
   }, [temperature])
 
   useEffect(() => {
-    localStorage.setItem('maxTokens', maxTokens)
+    localStorage.setItem("maxTokens", maxTokens)
   }, [maxTokens])
 
   useEffect(() => {
-    localStorage.setItem('userApiKey', userApiKey)
+    localStorage.setItem("userApiKey", userApiKey)
   }, [userApiKey])
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    if ((input === "") || (userApiKey === "")) {
-      return;
+    if (input === "" || userApiKey === "") {
+      return
     }
 
     // Update messages with the user message
     setMessages((prevMessages) => [
       ...prevMessages,
-      { role: 'user', content: input },
-    ]);
-    setInput('')
-  };
+      { role: "user", content: input },
+    ])
+    setInput("")
+  }
 
   useEffect(() => {
     if (messages.length > 0) {
-      if (messages[messages.length - 1].role === 'user') {
+      if (messages[messages.length - 1].role === "user") {
         callApi()
       }
     }
-  }, [messages]);
+  }, [messages])
 
   useEffect(() => {
     if (messagesContainerRef.current) {
-      messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight
     }
-  }, [messages]);
-  
+  }, [messages])
+
   useEffect(() => {
-    const newClientId = nanoid(); // Generate a unique clientId using nanoId
-    setClientId(newClientId);
-  
-    const eventSource = new EventSource(`https://chat-gpt-clone-y5ns.onrender.com/events/${newClientId}`);
-  
+    const newClientId = nanoid() // Generate a unique clientId using nanoId
+    setClientId(newClientId)
+
+    const eventSource = new EventSource(
+      `https://chat-gpt-clone-y5ns.onrender.com/events/${newClientId}`
+    )
+
     eventSource.onmessage = (event) => {
-      const data = JSON.parse(event.data);
+      const data = JSON.parse(event.data)
       console.log(data)
-  
+
       setMessages((prevMessages) => {
-        let lastMessage = prevMessages[prevMessages.length - 1];
-        let newMessages = prevMessages.slice(0, prevMessages.length - 1);
-      
-        if (lastMessage.role === 'assistant') {
-          let updatedMessage = { ...lastMessage, content: lastMessage.content + data };
-          newMessages.push(updatedMessage);
-        } 
-        else {
-          return [...prevMessages, { role: 'assistant', content: data }];
+        let lastMessage = prevMessages[prevMessages.length - 1]
+        let newMessages = prevMessages.slice(0, prevMessages.length - 1)
+
+        if (lastMessage.role === "assistant") {
+          let updatedMessage = {
+            ...lastMessage,
+            content: lastMessage.content + data,
+          }
+          newMessages.push(updatedMessage)
+        } else {
+          return [...prevMessages, { role: "assistant", content: data }]
         }
-        return newMessages;
-      });
-    };
-  
+        return newMessages
+      })
+    }
+
     return () => {
-      eventSource.close();
-    };
-  }, []);
+      eventSource.close()
+    }
+  }, [])
 
   const callApi = async () => {
-    await fetch('https://chat-gpt-clone-y5ns.onrender.com', {
-      method: 'POST',
+    await fetch("https://chat-gpt-clone-y5ns.onrender.com", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Messages: JSON.stringify(messages),
       },
       body: JSON.stringify({
@@ -113,90 +120,111 @@ export const Chat = () => {
         userApiKey: userApiKey,
         clientId: clientId,
         temperature: temperature,
-        maxTokens: maxTokens,
       }),
     })
   }
 
   useEffect(() => {
-
     const messagesHtml = messages.map((message, index) => (
-      <div key={index} style={message.role === "user"? {backgroundColor: '#1a1a1a'} : {backgroundColor: '#242424'}} className='message'>
-        <div className='message-body'>
-          <div className='image-container'>
-            <img src={message.role === "user"? "/avatar.png" : "/apple-touch-icon.png"} alt="user" className='message-image'/>
+      <div
+        key={index}
+        style={
+          message.role === "user"
+            ? { backgroundColor: "#1a1a1a" }
+            : { backgroundColor: "#242424" }
+        }
+        className="message"
+      >
+        <div className="message-body">
+          <div className="image-container">
+            <img
+              src={
+                message.role === "user"
+                  ? "/avatar.png"
+                  : "/apple-touch-icon.png"
+              }
+              alt="user"
+              className="message-image"
+            />
           </div>
-          <p className='user-message'>{message.content}</p>
+          <p className="user-message">{message.content}</p>
         </div>
       </div>
     ))
 
     setMessagesHtml(messagesHtml)
-  }, [messages]);
+  }, [messages])
 
-    const toggleMenu = () => {
-      const menu = document.querySelector(".aside");
-      const menu2 = document.querySelector(".aside-wrapper");
-      menu.classList.toggle("aside-active");
-      menu2.classList.toggle("aside-active");
-    };
+  const toggleMenu = () => {
+    const menu = document.querySelector(".aside")
+    const menu2 = document.querySelector(".aside-wrapper")
+    menu.classList.toggle("aside-active")
+    menu2.classList.toggle("aside-active")
+  }
 
   return (
-    <div className='container'>
-       <button className='hamburger-menu' onClick={toggleMenu}>
-        
-        <span className='hamburger-menu-line'><i className="fa-solid fa-bars"></i></span>
+    <div className="container">
+      <button className="hamburger-menu" onClick={toggleMenu}>
+        <span className="hamburger-menu-line">
+          <i className="fa-solid fa-bars"></i>
+        </span>
       </button>
-      <div className='main-content'>
-        <div className='aside-wrapper'>
-          <aside className='aside' >
-            <div className='dropdown'>
-              <Dropdown 
-              key={nanoid()}
-              label="Model" 
-              value={model}
-              options={[
-                { value: "gpt-4-0314", label: "GPT-4" },
-                { value: "gpt-3.5-turbo", label: "Chat-GPT" }
-              ]}
-              onChange={handleModelChange}
+      <div className="main-content">
+        <div className="aside-wrapper">
+          <aside className="aside">
+            <div className="dropdown">
+              <Dropdown
+                key={nanoid()}
+                label="Model"
+                value={model}
+                options={[
+                  { value: "gpt-4-0314", label: "GPT-4" },
+                  { value: "gpt-3.5-turbo", label: "Chat-GPT" },
+                ]}
+                onChange={changeModel}
               />
             </div>
 
             <p>Api Key</p>
-            <input type="password" value={userApiKey} onChange={(e) => setUserApiKey(e.target.value)} placeholder="Enter your api key"/>
+            <input
+              type="password"
+              value={userApiKey}
+              onChange={(e) => setUserApiKey(e.target.value)}
+              placeholder="Enter your api key"
+            />
 
-            <div className='temperature'>
+            <div className="temperature">
               <p>Temperature</p>
-              <Range state={temperature} handleChange={changeTemperature} min={0.1} max={1.0} />
+              <Range
+                state={temperature}
+                handleChange={changeTemperature}
+                min={0.1}
+                max={1.0}
+              />
             </div>
-
-            <div className='max-tokens'>
-              <p>Max Tokens</p>
-              <Range state={maxTokens} handleChange={changeMaxTokens} min={0} max={2048} />
-            </div>
-
           </aside>
         </div>
-        <section className='chat-log'>
+        <section className="chat-log">
           <h1>CHAT</h1>
-          <div className='messages-container' ref={messagesContainerRef}>
+          <div className="messages-container" ref={messagesContainerRef}>
             {messagesHtml}
           </div>
 
-          <form action="" method="post" onSubmit={handleSubmit} className='chat-form'>
-              <input type="text"
-                  name="input"
-                  placeholder="Type your message here"
-                  className='chat-input' 
-                  value={input}
-                  onChange={(e) => setInput(e.target.value)}
-              />
-              <input 
-                type="submit" 
-                value="Chat"
-                className='chat-submit'
-              />
+          <form
+            action=""
+            method="post"
+            onSubmit={handleSubmit}
+            className="chat-form"
+          >
+            <input
+              type="text"
+              name="input"
+              placeholder="Type your message here"
+              className="chat-input"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <input type="submit" value="Chat" className="chat-submit" />
           </form>
         </section>
       </div>
