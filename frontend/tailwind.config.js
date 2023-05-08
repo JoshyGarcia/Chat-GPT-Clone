25fde39e96ce48cd7f66982715404b0f84833323
Joshy Garcia <joshy.garcia@lc.cuny.edu>
2023-05-08 19:49:51 -0400

    install tailwind and flowbite

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
+}
